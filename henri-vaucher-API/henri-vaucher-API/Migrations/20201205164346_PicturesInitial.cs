a6633241ceba42619b4962ff1b9b2ef5e196233e
TA <tim.allemann@outlook.com>
2020-12-05 17:44:24 +0100

    First Migration

---
+++ henri-vaucher-API/henri-vaucher-API/Migrations/20201205164346_PicturesInitial.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace henri_vaucher_API.Migrations
+{
+    public partial class PicturesInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pictures",
+                columns: table => new
+                {
+                    PictureId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Date = table.Column<int>(type: "integer", nullable: false),
+                    Number = table.Column<int>(type: "integer", nullable: false),
+                    Signature = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    PositionSignature = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Height = table.Column<float>(type: "real", nullable: false),
+                    Width = table.Column<float>(type: "real", nullable: false),
+                    Surface = table.Column<float>(type: "real", nullable: false),
+                    Support = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Drawing = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    DominantTones = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Owner = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    From = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Remarks = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pictures", x => x.PictureId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pictures");
+        }
+    }
+}
