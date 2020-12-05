76673b91337e4998442cf5a53b48959bd16b511a
TA <tim.allemann@outlook.com>
2020-12-05 17:39:20 +0100

    Database initial

---
+++ henri-vaucher-API/henri-vaucher-API/Models/HenriVaucherContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace henri_vaucher_API.Models
+{
+    public class HenriVaucherContext : DbContext
+    {
+        public HenriVaucherContext(DbContextOptions<HenriVaucherContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Picture> Pictures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+    }
+}
---
+++ henri-vaucher-API/henri-vaucher-API/Models/Picture.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace henri_vaucher_API.Models
+{
+    public class Picture
+    {
+        public int PictureId { get; set; }
+        [StringLength(200)]
+        public string Title { get; set; }
+        public int Date { get; set; }
+        public int Number { get; set; }
+        [StringLength(200)]
+        public string Signature { get; set; }
+        [StringLength(200)]
+        public string PositionSignature { get; set; }
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public float Surface { get; set; }
+        [StringLength(200)]
+        public string Support { get; set; }
+        [StringLength(200)]
+        public string Drawing { get; set; }
+        [StringLength(200)]
+        public string DominantTones { get; set; }
+        [StringLength(200)]
+        public string Owner { get; set; }
+        [StringLength(200)]
+        public string From { get; set; }
+        [StringLength(200)]
+        public string Remarks { get; set; }
+    }
+}
